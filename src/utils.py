06f14c61c02509f474becebb5c181ff71bbfd106
santoshrajbhandari <newasantosh2001@gmail.com>
2025-04-20 14:34:47 +0545

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     """
@@ -20,15 +21,23 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models, param):
     """
     Evaluate the models and return the best model based on R2 score.
     """
     try:
         report = {}
+        
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(x_train, y_train)
+            para = param[list(models.keys())[i]]
+             
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
             train_model_score = r2_score(y_train, y_train_pred)
@@ -37,3 +46,14 @@ def evaluate_models(x_train, y_train, x_test, y_test, models):
         return report
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    """
+    Load the object from a file using pickle.
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)   
+    
